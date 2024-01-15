dc4c46da7af07d4e030286a062128cd1173e1b13
Ant <antoine.olivier@eleves.enpc.fr>
2024-01-15 01:05:36 +0100

    ok

--- code/algorithm_inv_prob.py
+++ code/algorithm_inv_prob.py
@@ -46,7 +46,8 @@ def univ_inv_sol(model, x_c ,task ,sig_0=1, sig_L=.01, h0=.01 , beta=.01 , freq=
     t=1
     start_time_total = time.time()
     while sigma > sig_L:
-
+        
+        # print("bornes:",y.min(),"   ",y.max())
 
         h = h0*t/(1+ (h0*(t-1)) )
         with torch.no_grad():
